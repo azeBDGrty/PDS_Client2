91a0c95536b7deb0231be288ea36b24a36bc6035
jihane <>
2016-05-18 10:44:47 +0200

    jihane

---
+++ src/com/pds/mvc_gestProspect/Controller_gestProspect.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pds.mvc_gestProspect;
+
+import com.pds.serverhandler.ConseillerHandle;
+
+/**
+ *
+ * @author zouhairhajji
+ */
+public class Controller_gestProspect {
+
+    private ConseillerHandle model;
+    
+    public Controller_gestProspect(ConseillerHandle model) {
+        this.model = model;
+    }
+    
+    
+    
+}
